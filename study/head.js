5757de7cb8e96a3169f3b18ce35ecd945920288c
LiuShuXi-Mini <Lsx@github.com>
2020-12-09 03:47:01 +0800

    1

--- study/head.js
+++ study/head.js
@@ -7,5 +7,5 @@ var m = [
 var a = 0
 var ele = document.getElementById("up")
 for (a in m) {
-    ele.innerHTML += "<a href='" + m[a][2] + "' class='a'>" + m[a][1] + "</a>"
+    ele.innerHTML += "<a href='" + m[a][1] + "' class='a'>" + m[a][0] + "</a>"
 }
