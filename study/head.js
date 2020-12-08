978f82b2f3e8310b74f4f10bddacc3e6ff893af0
LiuShuXi-Mini <Lsx@github.com>
2020-12-09 03:45:38 +0800

    1

---
+++ study/head.js
@@ -0,0 +1,11 @@
+var m = [
+    ["元素周期表" , "ele.html"],
+    ["中考倒计时", "text.html"],
+    ["相对原子质量" , "m.html"]
+]
+
+var a = 0
+var ele = document.getElementById("up")
+for (a in m) {
+    ele.innerHTML += "<a href='" + m[a][2] + "' class='a'>" + m[a][1] + "</a>"
+}
