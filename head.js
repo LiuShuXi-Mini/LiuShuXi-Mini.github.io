d4d07a50e9aa5c8f26082039236dc2db678c67d0
LiuShuXi-Mini <Lsx@github.com>
2020-12-31 12:34:04 +0800

    0

---
+++ head.js
@@ -0,0 +1,11 @@
+﻿var m = [
+    ["学习工具", "study"],
+    ["学习计划(建设中)", "#"]
+]
+
+var a = 0
+var ele = document.getElementById("up")
+for (a in m) {
+    ele.innerHTML += "<a href='" + m[a][1] + "' class='a'>" + m[a][0] + "</a>"
+}
+document.body.innerHTML += "<script src='/css/aos.js'></script>"
---
+++ study/head.js
@@ -0,0 +1,12 @@
+﻿var m = [
+    ["元素周期表" , "eles.html"],
+    ["中考倒计时", "text.html"],
+    ["相对原子质量" , "m.html"]
+]
+
+var a = 0
+var ele = document.getElementById("up")
+for (a in m) {
+    ele.innerHTML += "<a href='" + m[a][1] + "' class='a'>" + m[a][0] + "</a>"
+}
+document.body.innerHTML += "<script src='/css/aos.js'></script>"
